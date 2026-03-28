9d696eba4077cd1cacb01d662073dab69b4c4233
Pier Paolo Cirulli <ciru86@gmail.com>
2026-03-28 05:05:48 +0100

    Initial Commit

---
+++ SlideScribe/SlideScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideScribeApp.swift
+//  SlideScribe
+//
+//  Created by Pier Paolo Cirulli on 28/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SlideScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
